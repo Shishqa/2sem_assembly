8dc4cadde989f4cfe1b846558043378e90b8731f
shishqa <shishatskii.mn@phystech.edu>
2020-04-13 12:54:38 +0700

    NOMINAL -> deleted comment

--- C/Murmurhash64B/cmake-build-debug/CMakeFiles/Murmurhash64B.dir/DependInfo.cmake
+++ C/Murmurhash64B/cmake-build-debug/CMakeFiles/Murmurhash64B.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/shishqa/Repositories/MIPT/2-semester/Assembly_Dedinskiy/C/Murmurhash64B/main.c" "/home/shishqa/Repositories/MIPT/2-semester/Assembly_Dedinskiy/C/Murmurhash64B/cmake-build-debug/CMakeFiles/Murmurhash64B.dir/main.c.o"
+  "/home/shishqa/Repositories/MIPT/2020_2/asm/C/Murmurhash64B/main.c" "/home/shishqa/Repositories/MIPT/2020_2/asm/C/Murmurhash64B/cmake-build-debug/CMakeFiles/Murmurhash64B.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- C/Murmurhash64B/cmake-build-debug/cmake_install.cmake
+++ C/Murmurhash64B/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/shishqa/Repositories/MIPT/2-semester/Assembly_Dedinskiy/C/Murmurhash64B
+# Install script for directory: /home/shishqa/Repositories/MIPT/2020_2/asm/C/Murmurhash64B
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/shishqa/Repositories/MIPT/2-semester/Assembly_Dedinskiy/C/Murmurhash64B/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/shishqa/Repositories/MIPT/2020_2/asm/C/Murmurhash64B/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
